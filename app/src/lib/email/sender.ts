e5ef35d3c9847cd02d83e1f3b49a473d10f0799d
weaponsforge <forgeweapons@gmail.com>
2025-03-29 10:29:08 +0800

    feat: validate email content at runtime, #16

--- app/src/lib/email/sender.ts
+++ app/src/lib/email/sender.ts
@@ -3,8 +3,8 @@ dotenv.config()
 
 import EmailTransport from '@/lib/email/transport.js'
 import type { IEmailTransportAuth } from '@/types/transport.types.js'
-import type { ISendEmail } from '@/types/sender.types.js'
 import type { IEmailSender } from '@/types/sender.interface.js'
+import { type EmailType, EmailSchema } from '@/types/email.schema.js'
 
 /**
  * @class EmailSender
@@ -15,9 +15,15 @@ class EmailSender extends EmailTransport implements IEmailSender {
     super(params)
   }
 
-  async sendEmail (params: ISendEmail): Promise<void> {
+  async sendEmail (params: EmailType): Promise<void> {
     try {
       const transportOptions = this.getTransportOptions()
+      const result = EmailSchema.safeParse(params)
+
+      if (!result.success) {
+        throw new Error(result.error.message)
+      }
+
       const { recipient, subject, content } = params
 
       return await this.transporter!.sendMail({
