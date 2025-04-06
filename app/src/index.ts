29f16f5ccf7bd5027479b2e41681ea6cb31ccc9d
weaponsforge <forgeweapons@gmail.com>
2025-04-06 13:52:10 +0800

    chore: import/export schemavalidator class

--- app/src/index.ts
+++ app/src/index.ts
@@ -2,5 +2,6 @@ export {
   EmailSender,
   EmailTransport,
   GmailOAuthClient,
+  SchemaValidator,
   send
 } from '@/lib/index.js'
--- app/src/lib/index.ts
+++ app/src/lib/index.ts
@@ -1,11 +1,13 @@
 import EmailTransport from '@/lib/email/transport.js'
 import EmailSender from '@/lib/email/sender.js'
 import GmailOAuthClient from '@/lib/google/oauth2client.js'
+import SchemaValidator from '@/lib/validator/schemavalidator.js'
 
 export { send } from '@/lib/email/send.js'
 
 export {
   EmailSender,
   EmailTransport,
-  GmailOAuthClient
+  GmailOAuthClient,
+  SchemaValidator
 }
