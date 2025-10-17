32b731e6126ae969138a008f74483a8d5aac838c
weaponsforge <forgeweapons@gmail.com>
2025-10-18 01:01:34 +0800

    chore: detect cli exit code

--- app/src/__tests__/cli.test.ts
+++ app/src/__tests__/cli.test.ts
@@ -5,14 +5,19 @@ const MAX_TIMEOUT = 10000
 
 describe('CLI test suite', () => {
   test('Sends a text email using CLI options', async () => {
-    const { stdout } = await execa('npm', [
+    const { stdout, exitCode } = await execa('npm', [
       'run', 'send-email:dev', '--',
       'send',
       '-s \'Hello, World!\'',
       '-c \'Lorem ipsum dolor sit amet, consectetur adipiscing elit...\'',
       '-r tester@gmail.com'
-    ], { preferLocal: true })
+    ],
+    {
+      preferLocal: true,
+      reject: false
+    })
 
+    expect(exitCode).toBe(0)
     expect(stdout).toContain('Process success')
   }, MAX_TIMEOUT)
 })
