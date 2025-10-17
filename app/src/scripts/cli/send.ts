c6c3b402fedc52b98e2b568e35efc280de3dce2a
weaponsforge <forgeweapons@gmail.com>
2025-10-18 01:47:35 +0800

    chore: throw error instead of exit 0 in the cli

--- app/src/scripts/cli/send.ts
+++ app/src/scripts/cli/send.ts
@@ -8,36 +8,40 @@ import packageJson from '../../../package.json' with { type: 'json' }
 import { CLI_ARGS, CLI_META } from './meta.js'
 
 const program = new Command()
+  .exitOverride() // Throw errors instead of exiting
 
 program
   .name(CLI_META.PROGRAM.NAME)
   .description(CLI_META.PROGRAM.DESCRIPTION)
   .version(packageJson.version)
 
-// Send enail command
+// Send email command
 program.command(CLI_META.CMD_SEND.NAME)
   .description(CLI_META.CMD_SEND.DESCRIPTION)
   .requiredOption(CLI_ARGS.SUBJECT.OPTION, CLI_ARGS.SUBJECT.DESCRIPTION)
   .requiredOption(CLI_ARGS.CONTENT.OPTION, CLI_ARGS.CONTENT.DESCRIPTION)
   .requiredOption(CLI_ARGS.RECIPIENTS.OPTION, CLI_ARGS.RECIPIENTS.DESCRIPTION)
   .option(CLI_ARGS.ENV_FILE.OPTION, CLI_ARGS.ENV_FILE.DESCRIPTION)
-  .action(async (options) => {
-    let errorMsg
+  .action(async (options: Record<string, string>) => {
     const { subject, content, recipients, env } = options
 
     // Load user-provided .env file
     if (env) {
-      dotenv.config({ path: env })
+      dotenv.config({
+        path: env,
+        quiet: true
+      })
     }
 
     try {
-      if (!recipients) {
-        throw new Error('Invalid email recipients format')
-      }
-
       const emailRecipients = String(recipients)
         .split(',')
         .map(email => email.trim())
+        .filter(email => email.length > 0)
+
+      if (emailRecipients.length === 0) {
+        throw new Error('No valid email recipients provided')
+      }
 
       console.log(`Sending email to: ${recipients}`)
 
@@ -48,15 +52,12 @@ program.command(CLI_META.CMD_SEND.NAME)
       })
     } catch (err: unknown) {
       if (err instanceof Error) {
-        errorMsg = `[ERROR]: ${err.message}`
+        console.log(`[ERROR]: ${err.message}`)
+        throw err.message
       }
-    } finally {
-      const exitCode = errorMsg ? 1 : 0
-      const exitMsg = errorMsg ?? 'Process success'
-      console.log(exitMsg)
-
-      process.exit(exitCode)
     }
+
+    console.log('Process success')
   })
 
 /**
