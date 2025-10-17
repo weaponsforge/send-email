c44540ab758051717e33e11aa61b9722685e5280
weaponsforge <forgeweapons@gmail.com>
2025-10-18 00:52:30 +0800

    fix: call npm script with execa

--- app/src/__tests__/cli.test.ts
+++ app/src/__tests__/cli.test.ts
@@ -1,23 +1,17 @@
-import path from 'path'
 import { execa } from 'execa'
 import { describe, expect, test } from 'vitest'
-import { directory } from '@/utils/helpers.js'
 
 const MAX_TIMEOUT = 10000
 
-const cliPath = path.resolve(
-  directory(import.meta.url), '../scripts/cli/send.ts'
-)
-
 describe('CLI test suite', () => {
   test('Sends a text email using CLI options', async () => {
-    const { stdout } = await execa('npx vite-node', [
-      cliPath,
+    const { stdout } = await execa('npm', [
+      'run', 'send-email:dev', '--',
       'send',
       '-s \'Hello, World!\'',
       '-c \'Lorem ipsum dolor sit amet, consectetur adipiscing elit...\'',
       '-r tester@gmail.com'
-    ])
+    ], { preferLocal: true })
 
     expect(stdout).toContain('Process success')
   }, MAX_TIMEOUT)
