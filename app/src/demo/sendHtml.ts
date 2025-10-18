1036bd66f8b17dcc7b6b3dc3e6f12a179ec448c5
weaponsforge <forgeweapons@gmail.com>
2025-10-19 02:24:06 +0800

    chore: check for undefined env variable

--- app/src/demo/sendHtml.ts
+++ app/src/demo/sendHtml.ts
@@ -3,12 +3,16 @@ import { buildHtml } from '@/lib/index.js'
 
 import dataJson from '../templates/data.json' with { type: 'json' }
 
+if (!process.env.GOOGLE_USER_EMAIL) {
+  throw new Error('GOOGLE_USER_EMAIL .env variable is required')
+}
+
 // Build the HTML email content
 const emailContent = buildHtml({
   subject: dataJson.subject,
   content: [dataJson.content],
   recipients: dataJson.recipients,
-  sender: process.env.GOOGLE_USER_EMAIL
+  sender: process.env.GOOGLE_USER_EMAIL!
 })
 
 // Send the email
