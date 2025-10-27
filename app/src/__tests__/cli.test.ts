effd1b6f3a0cea1c45569f131765d19073be43b5
weaponsforge <forgeweapons@gmail.com>
2025-10-27 19:11:00 +0800

    feat: create cli tests - sending html/text email, #12

--- app/src/__tests__/cli.test.ts
+++ app/src/__tests__/cli.test.ts
@@ -1,9 +1,13 @@
-import { execa } from 'execa'
+import { $, execa } from 'execa'
 import { describe, expect, test } from 'vitest'
+import { EmailSchemaMessages } from '@/types/email.schema.js'
 
 const MAX_TIMEOUT = 10000
 
 describe('CLI test suite', () => {
+  /**
+   * ---------- Email sending ----------
+   */
   test('Sends a text email using CLI options', async () => {
     const { stdout } = await execa('npm', [
       'run', 'send-email:dev', '--',
@@ -17,5 +21,33 @@ describe('CLI test suite', () => {
     })
 
     expect(stdout).toContain('Process success')
+  }, MAX_TIMEOUT)
+
+  test('Sends the default HTML email using CLI options', async () => {
+    const correctEmailList = 'testor@gmail.com,abc@gmail.com'
+
+    const { stdout } = await $`npm run send-email:dev -- html
+    -s ${'Default HTML Message'}
+    -c ${'This is paragraph #1'} ${'This is paragraph #2'} ${'This is paragraph #3'}
+    -r ${correctEmailList}`
+
+    expect(stdout).toContain('Process success')
+  }, MAX_TIMEOUT)
+
+
+  /**
+   * ---------- CLI parameters ----------
+   */
+  test('Rejects email recipients list not separated by comma', async () => {
+    // Email list should be comma-separated
+    const incorrectEmailList = 'testor@gmail.com|abc@gmail.com'
+
+    const { stdout } = await $`npm run send-email:dev -- html
+      -s ${'Default HTML Message'}
+      -c ${'helloooo'}
+      -r ${incorrectEmailList}`
+
+    expect(stdout).toContain('[ERROR]')
+    expect(stdout).toContain(EmailSchemaMessages.RECIPIENT_EMAIL)
   }, MAX_TIMEOUT)
 })
